0115b3aeade62426c3e717cb175dd53528852ee4
iuliadrgn <iuliadragan02@yahoo.com>
2021-05-22 01:45:25 +0300

    home initial setup

---
+++ src/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Profile from "../pages/profile/Profile";
+import Contact from "../pages/contact/Contact";
+import About from "../pages/about/About";
+import Skills from "../pages/skills/Skills";
+import Education from "../pages/education/Education";
+import Projects from "../pages/projects/Projects"
+
+
+class Home extends Component {
+    render() {
+        return (
+            <section>
+                <div className="row">
+                    <div className="col s12 m12 l4 deep-purple darken-3">
+                        <Profile/>
+                        <Contact/>
+                        <Skills/>
+
+                    </div>
+                    <div className="col s12 m12 l8 white">
+                        <About/>
+                        <Education/>
+                        <Projects/>
+
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Home;
